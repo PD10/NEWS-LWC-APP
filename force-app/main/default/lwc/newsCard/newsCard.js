b2c9dd0237e589f12a6acfec5fe817cf13cc66d9
pritish r das <pritish.r.das@apiseroinc.onmicrosoft.com>
2022-06-18 21:34:18 +0530

    Adding navigation to site

--- force-app/main/default/lwc/newsCard/newsCard.js
+++ force-app/main/default/lwc/newsCard/newsCard.js
@@ -1,5 +1,15 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class NewsCard extends LightningElement {
+export default class NewsCard extends NavigationMixin(LightningElement) {
     @api newsItem;
+
+    navigateToWebPage() {
+        this[NavigationMixin.Navigate]({
+            "type": "standard__webPage",
+            "attributes": {
+                "url": this.newsItem.url
+            }
+        });
+    }
 }
